cefaa1f3fe18076b2aead3f6b42b01056c68ee48
astro <yuleiz@gmail.com>
2024-04-25 10:52:56 +0800

    pop to 0.5.0 added animation file converter

--- src/main/python/amk/animation.py
+++ src/main/python/amk/animation.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QLabel, QFileDialog, QListWidget, QProgressBar
-from PyQt5.QtCore import QSize, QRect, QPoint, Qt
+from PyQt5.QtCore import QSize, QRect, QPoint, Qt, pyqtSignal, QObject, QTimer
+
 from PyQt5.QtGui import QPainter, QBrush, QColor, QImage, QPixmap, QImageReader, QMovie
 
 from themes import Theme
@@ -45,11 +46,190 @@ ANIM_MODES = {
     "abkg_240_120": {"width": 240, "height": 120, "magic": "ABKG"},
 }
 
+KEYBOARD_FORMATS = [
+    {"name": "Navi", "mode": "anim_128_128", "suffix": ".crs", "filter": "Navi Files (*.crs)"},
+    {"name": "Corsa", "mode": "anim_128_128", "suffix": ".crs", "filter": "Corsa Files (*.crs)"},
+    {"name": "Meta Background", "mode": "abkg_160_80", "suffix": ".bkg", "filter": "Meta Backaground Files (*.bkg)"},
+    {"name": "Meta Status", "mode": "asts_80_30", "suffix": ".sts", "filter": "Meta Status Files (*.sts)"},
+    {"name": "Meta Right", "mode": "anim_60_60", "suffix": ".sml", "filter": "Meta Right Files (*.sml)"},
+    {"name": "Meta Up Left", "mode": "auxi_80_30", "suffix": ".aux","filter": "Meta Up Left Files (*.aux)"},
+    {"name": "8XV3.0", "mode": "anim_60_60", "suffix": ".sml","filter": "8xv3.0 Files (*.sml)"},
+    {"name": "Vita Up", "mode": "anim_60_60", "suffix": ".anm","filter": "Vita Up Files (*.anm)"},
+    {"name": "Vita Down", "mode": "auxi_80_30", "suffix": ".aux","filter": "Vita Down Files (*.aux)"},
+]
+
 ANIM_HDR = "<4s2HI4H"
 
 FILE_FILTER_IMAGES="Images (*.jpg *.png *.bmp)"
 FILE_FILTER_ANIMATIONS="Animations (*.gif)"
 FILE_FILTER_AMK="Converted Files (*.anm *.sml *.bkg *.crs *.sts)"
+
+def convert_frame(image, dst_width, dst_height, file_name):
+    image_width = image.rect().width()
+    image_height = image.rect().height()
+    src_aspect = image_width/image_height
+    dst_aspect = dst_width/dst_height
+
+    if dst_aspect < src_aspect:
+        scale = dst_width / image_width
+        scale_size = (dst_width, int(image_height*scale))
+        origin_point = QPoint(0, int(abs(dst_height - image_height*scale)/2))
+    else:
+        scale = dst_height / image_height
+        scale_size = (int(image_width*scale), dst_height)
+        origin_point = QPoint(int(abs(dst_width - image_width*scale)/2), 0)
+    scaled_img = image.scaled(scale_size[0], scale_size[1],  transformMode=Qt.SmoothTransformation)
+    dst_img = QImage(dst_width, dst_height, QImage.Format_RGB888)
+    dst_img.fill(Qt.black)
+    pt = QPainter(dst_img)
+    pt.drawImage(origin_point, scaled_img)
+    pt.end()
+
+    if file_name is not None:
+        dst_img.save(file_name)
+
+    return dst_img 
+
+def extract_frame_data(image):
+    data = bytearray()
+    width = image.rect().width()
+    height = image.rect().height()
+    for h in range(height):
+        for w in range(width):
+            r = image.pixelColor(w,h).red()
+            g = image.pixelColor(w,h).green()
+            b = image.pixelColor(w,h).blue()
+            data = data + struct.pack(">H", (r >> 3) << 11 | (g >> 2) << 5 | (b >> 3) )
+    return data
+
+def pack_anim_header(width, height, magic, total):
+    hdr_size = struct.calcsize(ANIM_HDR)
+    offset = hdr_size + 2*total
+    file_size = offset + total*width*height*2
+    sig = bytes(magic, "utf-8")
+
+    #pack file header
+    data = struct.pack(ANIM_HDR, sig, hdr_size, offset, file_size, width, height,2,total)
+    return data
+
+class AmkMovie(QObject):
+    update_frame = pyqtSignal()
+    def __init__(self, file_name):
+        super().__init__()
+
+        self.file_name = file_name
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.index = 0
+        self.frames = []
+        self.sig = ""
+        self.width = 0
+        self.height = 0
+
+    def convert_frame_to_image(self, frame):
+        img = QImage(self.width, self.height, QImage.Format_RGB888)
+        for h in range(self.height):
+            for w in range(self.width):
+                offset = h*self.width+w
+                rgb565, = struct.unpack(">H", frame[offset*2:offset*2+2])
+                red = ((rgb565 >> 11) & 0x1F) << 3
+                green = ((rgb565 >> 5) & 0x3F) << 2
+                blue = ((rgb565) & 0x1F) << 3
+                c = QColor.fromRgb(red, green, blue)
+                img.setPixelColor(w,h,c)
+
+        return img
+
+
+    def parse(self):
+        if self.file_name is None:
+            return False
+
+        with open(self.file_name, "rb") as f:
+            data = f.read()
+            hdr_size = struct.calcsize(ANIM_HDR)
+            if len(data) < hdr_size:
+                return False
+
+            sig, hdr_size, offset, file_size, self.width, self.height, format, total = struct.unpack(ANIM_HDR, data[:hdr_size])
+            self.sig = sig.decode("utf-8")
+
+            found = False
+            for k, v in ANIM_MODES.items():
+                if self.sig == v["magic"] and self.width == v["width"] and self.height == v["height"]:
+                    self.mode = k 
+                    found = True
+                    break
+            if not found:
+                #print("not a valid amk animation file")
+                return False
+            if len(data) < hdr_size+total*2+total*self.width*self.height*2:
+                #print("File size not valid")
+                return False
+
+            frame_size = self.width*self.height*2
+            for i in range(total):
+                start = offset+frame_size*i
+                frame_data = data[start:start+frame_size]
+                frame_delay, = struct.unpack("<H", data[hdr_size+2*i:hdr_size+2*i+2])
+                self.frames.append({"frame":self.convert_frame_to_image(frame_data), "delay":frame_delay})
+            
+            return True
+    
+    def get_current_image(self):
+        if self.index < len(self.frames):
+            return self.frames[self.index]
+        return None
+
+    def total(self):
+        return len(self.frames)
+
+    def get_image(self, index):
+        if index < self.total():
+            return self.frames[index]["frame"]
+
+        return None
+    
+    def get_delay(self, index):
+        if index < self.total():
+            return self.frames[index]["delay"]
+
+        return 0
+
+    def pack_animation(self, dst_sig, dst_width, dst_height, file_name):
+        if self.total() == 0:
+            return None
+
+        total = len(self.frames)
+        #pack header
+        data = pack_anim_header(dst_width, dst_height, dst_sig, total)
+        #pack delay
+        for i in range(total):
+            data = data + struct.pack("<H", self.frames[i]["delay"])
+        #pack frame
+        for i in range(total):
+            img = self.get_image(i)
+            frame = convert_frame(img, dst_width, dst_height, None)
+            data = data + extract_frame_data(frame)
+        
+        if file_name is not None:
+            with open(file_name, "wb") as f:
+                f.write(data)
+        
+        return data
+
+    def rect(self):
+        return QRect(0,0,self.width,self.height)
+
+    def update(self):
+        self.index = (self.index+1) % len(self.frames)
+        self.update_frame.emit()
+        self.timer.start(self.frames[self.index]["delay"])
+    
+    def start(self):
+        self.index = 0
+        self.timer.start(self.frames[self.index]["delay"])
+
 
 class Animation(BasicEditor):
     def __init__(self):
@@ -99,15 +279,19 @@ class Animation(BasicEditor):
         self.download_lst.setMaximumSize(QSize(200, 80))
         v_layout_in.addWidget(self.download_lst)
         self.refresh_btn = QPushButton("Refresh")
+        self.refresh_btn.setEnabled(False)
         self.refresh_btn.clicked.connect(self.on_refresh_btn_clicked)
         v_layout_in.addWidget(self.refresh_btn)
         self.delete_btn = QPushButton("Delete ...")
+        self.delete_btn.setEnabled(False)
         self.delete_btn.clicked.connect(self.on_delete_btn_clicked)
         v_layout_in.addWidget(self.delete_btn)
         self.copy_btn = QPushButton("Copy ...")
+        self.copy_btn.setEnabled(False)
         self.copy_btn.clicked.connect(self.on_copy_btn_clicked)
         v_layout_in.addWidget(self.copy_btn)
         self.download_btn = QPushButton("Download ...")
+        self.download_btn.setEnabled(False)
         self.download_btn.clicked.connect(self.on_download_btn_clicked)
         v_layout_in.addWidget(self.download_btn)
         self.download_bar = QProgressBar()
@@ -138,15 +322,9 @@ class Animation(BasicEditor):
     def rebuild_ui(self):
         self.clear_animation_ui()
 
-        self.convert_lst.addItem("Navi")
-        self.convert_lst.addItem("Corsa")
-        self.convert_lst.addItem("Meta Background")
-        self.convert_lst.addItem("Meta Status")
-        self.convert_lst.addItem("Meta Right")
-        self.convert_lst.addItem("Meta Up Left")
-        self.convert_lst.addItem("8XV3.0")
-        self.convert_lst.addItem("Vita Up")
-        self.convert_lst.addItem("Vita Down")
+        for format in KEYBOARD_FORMATS:
+            self.convert_lst.addItem(format["name"])
+
         self.convert_lst.setCurrentRow(0)
 
     def rebuild(self, device):
@@ -189,7 +367,6 @@ class Animation(BasicEditor):
                 if int((total-remain)*100/ total) > progress:
                     progress = int((total-remain)*100/total) 
                     self.download_bar.setValue(progress)
-                print(remain, cur)
 
             self.keyboard.close_anim_file(index)
 
@@ -198,6 +375,20 @@ class Animation(BasicEditor):
 
     def on_copy_btn_clicked(self):
         pass
+
+    def update_animation_display(self):
+        pixmap = QPixmap.fromImage(self.player.get_current_image()["frame"])
+        if pixmap is None:
+            return
+
+        label_size = self.display_label.frameSize()
+        pixmap_rect = self.player.rect()
+        if pixmap_rect.width() > label_size.width() or pixmap_rect.height() > label_size.height():
+            pixmap = pixmap.scaled(label_size.width(), label_size.height(), aspectRatioMode=Qt.KeepAspectRatio)
+        self.display_label.setPixmap(pixmap)
+
+    def on_animation_update(self):
+        self.update_animation_display()
 
     def on_select_btn_clicked(self):
         dialog = QFileDialog()
@@ -228,87 +419,60 @@ class Animation(BasicEditor):
                         pixmap = pixmap.scaled(label_size.width(), label_size.height(), aspectRatioMode=Qt.KeepAspectRatio)
                     self.display_label.setPixmap(pixmap)
                 else:
-                    pass
+                    self.player = AmkMovie(self.current_file)
+                    if self.player.parse():
+                        self.update_animation_display()
+                        self.player.update_frame.connect(self.on_animation_update)
+                        self.player.start()
     
     def on_convert_btn_clicked(self):
         if self.current_file is not None:
-            fileName, _ = QFileDialog.getSaveFileName(None, "Save File", "", "Corsa Files(*.crs)")
-            if fileName:
-                self.covert_animation(self.current_file, fileName, "anim_128_128")
-
-    def convert_frame(self, image, mode, file_name):
-        image_width = image.rect().width()
-        image_height = image.rect().height()
-        src_aspect = image_width/image_height
-        dst_aspect = mode["width"]/mode["height"]
-
-        if dst_aspect < src_aspect:
-            scale = mode["width"] / image_width
-            scale_size = (mode["width"], int(image_height*scale))
-            origin_point = QPoint(0, int(abs(mode["height"] - image_height*scale)/2))
-        else:
-            scale = mode["height"] / image_height
-            scale_size = (int(image_width*scale), mode["height"])
-            origin_point = QPoint(int(abs(mode["width"] - image_width*scale)/2), 0)
-        scaled_img = image.scaled(scale_size[0], scale_size[1],  transformMode=Qt.SmoothTransformation)
-        dst_img = QImage(mode["width"], mode["height"], QImage.Format_RGB888)
-        dst_img.fill(Qt.black)
-        pt = QPainter(dst_img)
-        print(origin_point)
-        pt.drawImage(origin_point, scaled_img)
-        pt.end()
-
-        if file_name is not None:
-            dst_img.save(file_name)
-
-        return dst_img 
-
-    def extract_frame_data(self, image):
-        data = bytearray()
-        width = image.rect().width()
-        height = image.rect().height()
-        for h in range(height):
-            for w in range(width):
-                r = image.pixelColor(w,h).red()
-                g = image.pixelColor(w,h).green()
-                b = image.pixelColor(w,h).blue()
-                data = data + struct.pack(">H", (r >> 3) << 11 | (g >> 2) << 5 | (b >> 3) )
-        return data
+            name = self.convert_lst.currentItem().text()
+            for format in KEYBOARD_FORMATS:
+                if name == format["name"]:
+                    fileName, _ = QFileDialog.getSaveFileName(None, "Save File", "", format["filter"])
+                    if fileName:
+                        self.pack_animation_to_file(self.current_file, fileName, format["mode"])
+                    break
 
     def extract_frames(self, src, mode):
         frames = []
 
         if self.current_filter == FILE_FILTER_ANIMATIONS:
             reader = QMovie(src)
-            print("Image frame count: ", reader.frameCount())
             for i in range(reader.frameCount()):
                 if reader.jumpToFrame(i):
-                    print("Current number: ", reader.currentFrameNumber())
                     img = reader.currentImage()
                     #file_name = "e:\\work\\vial\\{}.bmp".format(i)
-                    frame = self.convert_frame(img, mode, None)
-                    data = self.extract_frame_data(frame)
+                    frame = convert_frame(img, mode["width"], mode["height"], None)
+                    data = extract_frame_data(frame)
                     delay = reader.nextFrameDelay()
                     frames.append({"data":data, "delay":delay})
-                else:
-                    print("failed to jump: ", i)
-            
 
         if self.current_filter == FILE_FILTER_IMAGES:
             reader = QImageReader(src)
             if reader.canRead():
                 img = reader.read()
-                frame = self.convert_frame(img, mode, None)
-                data = self.extract_frame_data(frame)
+                frame = convert_frame(img, mode["width"], mode["height"], None)
+                data = extract_frame_data(frame)
                 delay = 0
                 frames.append({"data":data, "delay":delay})
+        
+        if self.current_filter == FILE_FILTER_AMK:
+            reader = AmkMovie(src)
+            if reader.parse():
+                for i in range(reader.total()):
+                    img = reader.get_image(i)
+                    frame = convert_frame(img, mode["width"], mode["height"], None)
+                    data = extract_frame_data(frame)
+                    delay = reader.get_delay(i)
+                    frames.append({"data":data, "delay":delay})
 
         return frames
 
-    def pack_animation_in_memory(self, src, format):
+    def pack_animation_in_memory(self, src, format, file_name=None):
         mode = ANIM_MODES.get(format)
         if mode is None:
-            print("Unsupport format: ", format)
             return None 
 
         frames = self.extract_frames(src, mode)
@@ -316,13 +480,8 @@ class Animation(BasicEditor):
         if total == 0:
             return None
 
-        hdr_size = struct.calcsize(ANIM_HDR)
-        offset = hdr_size + 2*total
-        file_size = offset + total*mode["width"]*mode["height"]*2
-        sig = bytes(mode["magic"], "utf-8")
-
         #pack file header
-        data = struct.pack(ANIM_HDR, sig, hdr_size, offset, file_size, mode["width"], mode["height"],2,total)
+        data = pack_anim_header(mode["width"], mode["height"], mode["magic"], total)
         #pack frame durations
         for i in range(total):
             data = data + struct.pack("<H", frames[i]["delay"])
@@ -330,28 +489,24 @@ class Animation(BasicEditor):
         for i in range(total):
             data = data + frames[i]["data"]
         
-        with open("e:\\work\\vial\\ccc.crs", "wb") as f:
-            f.write(data)
+        if file_name is not None:
+            with open(file_name) as f:
+                f.write(data)
 
         return data
 
-    def covert_animation(self, src, dst, format):
+    def pack_animation_to_file(self, src, dst, format):
         mode = ANIM_MODES.get(format)
         if mode is None:
-            print("Unsupport format: ", format)
             return
 
         frames = self.extract_frames(src, mode)
         if len(frames) > 0:
             with open(dst, "wb") as f:
                 total = len(frames)
-                hdr_size = struct.calcsize(ANIM_HDR)
-                offset = hdr_size + 2*total
-                file_size = offset + total*mode["width"]*mode["height"]*2
-                sig = bytes(mode["magic"], "utf-8")
-                data = struct.pack(ANIM_HDR, sig, hdr_size, offset, file_size, mode["width"], mode["height"],2,total)
 
                 #write file header
+                data = pack_anim_header(mode["width"], mode["height"], mode["magic"], total)
                 f.write(data)
                 #write frame durations
                 for i in range(total):
@@ -360,8 +515,6 @@ class Animation(BasicEditor):
                 #write frame data
                 for i in range(total):
                     f.write(frames[i]["data"])
-        else:
-            print("Failed to extract frames from: ", src)
 
     def generate_filename(self, name):
         append_tilde = True
