034d4e0196b2f1da22743f09beebb86b927d5767
astro <yuleiz@gmail.com>
2024-11-26 17:24:49 +0800

    polulate amk version to 0.9.1

--- src/main/python/amk/protocol.py
+++ src/main/python/amk/protocol.py
@@ -4,7 +4,7 @@ from keycodes.keycodes import Keycode
 
 from protocol.base_protocol import BaseProtocol
 
-AMK_VERSION = "0.8.1"
+AMK_VERSION = "0.9.1"
 
 AMK_PROTOCOL_PREFIX = 0xFD
 AMK_PROTOCOL_OK = 0xAA
