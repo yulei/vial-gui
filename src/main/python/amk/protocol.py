06f780283bb62393704b25ca4859e091db93ea0c
astro <yuleiz@gmail.com>
2025-07-26 09:50:29 +0800

    update amk version

--- src/main/python/amk/protocol.py
+++ src/main/python/amk/protocol.py
@@ -4,7 +4,7 @@ from keycodes.keycodes import Keycode
 
 from protocol.base_protocol import BaseProtocol
 
-AMK_VERSION = "0.9.9"
+AMK_VERSION = "0.9.10"
 
 AMK_PROTOCOL_PREFIX = 0xFD
 AMK_PROTOCOL_OK = 0xAA
